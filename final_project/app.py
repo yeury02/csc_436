d8e8b09eeae0b4bc210e39cbbc748a95f2c7d0c6
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-03-04 08:23:22 -0800

    handled students putting the same passwords

--- final_project/app.py
+++ final_project/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template,request, flash
+from passlib.hash import sha256_crypt
 
 
 app = Flask(__name__)
@@ -17,10 +17,14 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
         confirm = request.form.get("confirm")
-        secure_password = sha256_scrypt.encrypt(str(password))
+        secure_password = sha256_crypt.encrypt(str(password))
         
         if password == confirm:
             # insert into database here!
+            pass
+        else:
+            flash("password does not match", "danger")
+            return render_template('register.html')
     return render_template('register.html')
 
 @app.route('/login')
@@ -36,4 +40,5 @@ def login():
 
 
 if __name__ == '__main__':
+    app.secret_key = "1234567csc436finalproject"
     app.run(debug=True)
