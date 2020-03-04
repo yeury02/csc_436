2a215a27b3fdb17c7cc2ee1c4410f434ba6c2d77
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-03-04 07:33:32 -0800

    added new file

--- final_project/app.py
+++ final_project/app.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/home')
-def hello_world():
-    return render_template('index.html')
+# @app.route('/home')
+def home():
+    return render_template('home.html')
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+        secure_password = sha256_scrypt.encrypt(str(password))
+        
+        if password == confirm:
+            # insert into database here!
     return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 
 # @app.route('/about')
 # def about():
