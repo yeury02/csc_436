b593f5bee1aea91812b441d737bf05abe35e2d3d
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-03-04 21:51:13 -0800

    working on connecting mysql with python

--- final_project/app.py
+++ final_project/app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template,request, flash
+from flask import Flask, render_template,request,flash, url_for, redirect
 from passlib.hash import sha256_crypt
+from flask_mysqldb import MySQL
+# import yaml
+
 
 
 app = Flask(__name__)
+
+#configure db
+#db = yaml.load(open('db.yaml'))
+app.config['MYSQL_USER'] = 'sql9325675'
+app.config['MYSQL_PASSWORD'] = 'fTXVhmuuYA'
+app.config['MYSQL_HOST'] = 'sql9.freemysqlhosting.net'
+app.config['MYSQL_DB'] = 'sql9325675'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
 
 
 @app.route('/')
@@ -18,9 +31,19 @@ def register():
         password = request.form.get("password")
         confirm = request.form.get("confirm")
         secure_password = sha256_crypt.encrypt(str(password))
+        flag = True
         
         if password == confirm:
             # insert into database here!
+            cur = mysql.connection.cursor()
+            if flag:
+                cur.execute('''CREATE TABLE users (name varchar(100) not null, username varchar(100) not null, password varchar(100) not null))''')
+                flag = False
+            cur.execute('''INSERT INTO users(name,username,password) VALUES(%s, %s, %s)", (name, username, password)''')
+            mysql.connection.commit()
+            cur.close()
+            flash("you are registered and can login", "success")
+            return redirect(url_for('login'))
             pass
         else:
             flash("password does not match", "danger")
