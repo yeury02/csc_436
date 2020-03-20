3d53be3b1718172501bae9f4dd8cdba8d3bb0d70
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-03-19 22:56:32 -0700

    added file, downloaded some packages

--- final_project/app.py
+++ final_project/app.py
@@ -1,23 +1,20 @@
 from flask import Flask, render_template,request,flash, url_for, redirect
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
-
+import yaml
 
 app = Flask(__name__)
 
 #configure db
-# app.config['MYSQL_USER'] = 'sql9325675'
-# app.config['MYSQL_PASSWORD'] = 'fTXVhmuuYA'
-# app.config['MYSQL_HOST'] = 'sql9.freemysqlhosting.net'
-# app.config['MYSQL_DB'] = 'sql9325675'
-# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '@Yeury026'
-app.config['MYSQL_DB'] = 'register'
+db.yaml.load(open(db.yaml))
+app.config['MYSQL_HOST'] = 
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
 
-mysql = MySQL(app)
+
+# mysql = MySQL(app)
 
 
 @app.route('/')
@@ -33,22 +30,23 @@ def register():
         password = request.form.get("password")
         confirm = request.form.get("confirm")
         secure_password = sha256_crypt.encrypt(str(password))
-        #flag = True
         
         if password == confirm:
-            # insert into database here!
-            cur = mysql.connection.cursor()
-            # if flag:
-            #     cur.execute('''CREATE TABLE users (
-            #                         name varchar(100) not null, 
-            #                         username varchar(100) not null, 
-            #                         password varchar(100) not null
-            #                         )''')
-            #     flag = False
-            cur.execute("INSERT INTO users(name,username,password) VALUES(:name, :username, :secure_password)",
-                                           {"name":name, "username":username, "password":secure_password})
-            mysql.connection.commit()
-            #cur.close()
+            db.execute("INSERT INTO users(name, username, password) VALUES(:name, :username, :password)",
+                                            {"name":name,"username":username,"password":secure_password})
+            db.commit()
+            # # insert into database here!
+            # cur = mysql.connection.cursor()
+            # # if flag:
+            # #     cur.execute('''CREATE TABLE users (
+            # #                         name varchar(100) not null, 
+            # #                         username varchar(100) not null, 
+            # #                         password varchar(100) not null
+            # #                         )''')
+            # #     flag = False
+            # cur.execute("INSERT INTO users(name,username,password) VALUES(name, username, secure_password)")
+            # mysql.connection.commit() 
+            # cur.close()
             flash("you are registered and can login", "success")
             return redirect(url_for('login'))
         else:
